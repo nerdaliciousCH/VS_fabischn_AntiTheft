3b1aac620f4972fa28a7694a2e36b4505651ad3c
Olivier Bitter <Olivier Bitter>
2016-10-07 19:45:43 +0200

    Add SettingsActivity and SettingsFragment

---
+++ app/src/main/java/ch/ethz/inf/vs/a1/fabischn/antitheft/SettingsFragment.java
@@ -0,0 +1,16 @@
+package ch.ethz.inf.vs.a1.fabischn.antitheft;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+
+
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
