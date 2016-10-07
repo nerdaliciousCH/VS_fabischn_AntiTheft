3d46d6888a21b3e0c4262edcd866fd7d71980dc3
Olivier Bitter <Olivier Bitter>
2016-10-07 19:50:33 +0200

    Add options menu

--- app/src/main/java/ch/ethz/inf/vs/a1/fabischn/antitheft/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/fabischn/antitheft/MainActivity.java
@@ -1,8 +1,12 @@
 package ch.ethz.inf.vs.a1.fabischn.antitheft;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ToggleButton;
 
@@ -28,5 +32,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_actuators, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                Intent myIntent = new Intent(this, SettingsActivity.class);
+                this.startActivity(myIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
 }
--- app/src/main/java/ch/ethz/inf/vs/a1/fabischn/antitheft/SettingsActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/fabischn/antitheft/SettingsActivity.java
@@ -8,6 +8,8 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
