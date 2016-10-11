54df9cb00b9730d3661153c0ed5b304a13ee1771
Olivier Bitter <Olivier Bitter>
2016-10-11 22:15:08 +0200

    Add seekbars

--- app/src/main/java/ch/ethz/inf/vs/a1/fabischn/antitheft/SeekbarPreference.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/fabischn/antitheft/SeekbarPreference.java
@@ -1,23 +1,83 @@
 package ch.ethz.inf.vs.a1.fabischn.antitheft;
 
-
 import android.content.Context;
-import android.preference.DialogPreference;
+import android.content.res.TypedArray;
 import android.preference.Preference;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class SeekbarPreference extends Preference {
+public class SeekbarPreference extends Preference implements OnSeekBarChangeListener {
+    private SeekBar mSeekBar;
+    private int mProgress;
 
     public SeekbarPreference(Context context) {
-        super(context);
+        this(context, null, 0);
     }
+
+    public SeekbarPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SeekbarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setLayoutResource(R.layout.seekbar_preference);
+    }
+
 
     @Override
     protected View onCreateView(ViewGroup parent) {
-        LayoutInflater li = (LayoutInflater)getContext().getSystemService( Context.LAYOUT_INFLATER_SERVICE );
-        return li.inflate( R.layout.seekbar_preference, parent, false);
+        return super.onCreateView(parent);
+
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        mSeekBar = (SeekBar) view.findViewById(R.id.seekbar);
+        mSeekBar.setProgress(mProgress);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (!fromUser)
+            return;
+
+        setValue(progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // not used
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // not used
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        setValue(restoreValue ? getPersistedInt(mProgress) : (Integer) defaultValue);
+    }
+
+    public void setValue(int value) {
+        if (shouldPersist()) {
+            persistInt(value);
+        }
+
+        if (value != mProgress) {
+            mProgress = value;
+            notifyChanged();
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getInt(index, 0);
     }
 }
+
